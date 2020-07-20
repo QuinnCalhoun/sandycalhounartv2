2edb24de907e7f766dee18d84492902335cdc63f
Quinn Calhoun <quinn.tcalhoun@gmail.com>
2020-07-19 17:55:44 -0700

    for got sakes

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    USER: 'sandycalhounartworks@gmail.com', 
-    PASS: '4385.Gabi',
-    CLIENTID: '697946915877-oibq583a142r7q9lsf519tnea35qm0ps.apps.googleusercontent.com',
-    CLIENTSECRET: 'd5WcNqegJDItPyA9Zt95Q7F3',
-}
