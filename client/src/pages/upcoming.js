17807f95ed3197de1d82d66b00a306cc5b2d0ab5
Quinn Calhoun <Quinn.TCalhoun@gmail.com>
2020-07-24 09:32:45 -0700

    uploading the upcoming page

--- client/src/pages/upcoming.js
+++ client/src/pages/upcoming.js
@@ -1,8 +1,11 @@
 import React from 'react' 
+import { Image } from 'semantic-ui-react'
  
 const Upcoming = () => {
     return (
-        <div>Ah, General Kenobi</div>
+        <>
+            <Image src="https://static.wixstatic.com/media/cfa348_c1421d0539c94377874105e9878e03bd~mv2.jpg/v1/fill/w_898,h_1118,al_c,q_85,usm_0.66_1.00_0.01/666%20poster%20website.webp" alt='Oakland Applied Contemporary' />
+        </>
     )
 }
 
