b8a3d4995df222773549e2ec03b988074460e487
Quinn Calhoun <quinn.tcalhoun@gmail.com>
2020-08-01 01:04:52 -0700

    f

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
     const [message, setMessage] = useState('')
     const [send, setSending] = useState('Send Message')
 
-    const emailer = ( event ) => {
+    const emailer = (event) => {
         event.preventDefault()
         setSending('...Sending')
         const data = {
@@ -18,17 +18,20 @@ const Contact = () => {
             subject: subject,
             message: message
         }
-        axios.post('https://www.sandycalhoun.com/api/contact', data)
-            .then(res => {
-                console.log(res)
-                if (res.status === 200) {
-                    console.log('biggedy boi')
-                    setSending('Message Sent')
-                    setTimeout(() => {
-                        resetForm()
-                    }, 500) 
-                }
-            })
+        axios({
+            method: 'post',
+            url: 'https://www.sandycalhoun.com/api/contact',
+            withCredentials: true
+        }).then(res => {
+            console.log(res)
+            if (res.status === 200) {
+                console.log('biggedy boi')
+                setSending('Message Sent')
+                setTimeout(() => {
+                    resetForm()
+                }, 500)
+            }
+        })
             .catch(() => {
                 setSending('Error, message not sent')
             })
@@ -42,32 +45,32 @@ const Contact = () => {
         setSending('Send Message')
     }
 
-return (
-    <>
-        <Header as='h1' content='Contact' />
-        <Grid columns='2' stackable >
-            <Grid.Column width='6'>
-                <p>
-                    Please feel free to reach out with any questions regarding shows, purchases, or commissions
+    return (
+        <>
+            <Header as='h1' content='Contact' />
+            <Grid columns='2' stackable >
+                <Grid.Column width='6'>
+                    <p>
+                        Please feel free to reach out with any questions regarding shows, purchases, or commissions
                     </p>
-                <Segment>
-                    <Form>
-                        <Form.Input onChange={(e, d) => setName(d.value)} value={name} placeholder='Name' />
-                        <Form.Input onChange={(e, d) => setEmail(d.value)} value={email} placeholder='Email' />
-                        <Form.Input onChange={(e, d) => setSubject(d.value)} value={subject} placeholder='Subject' />
-                        <TextArea onChange={(e, d) => setMessage(d.value)} value={message} class='field' placeholder='Message' style={{ height: '150px' }} />
-                        <Form.Button content={send} onClick={emailer} type='Submit' />
-                    </Form>
-                </Segment>
-            </Grid.Column>
-            <Grid.Column width='10'>
-                <Image src={require('../assets/pics/Lookout.jpg')} attached='top' />
-            </Grid.Column>
+                    <Segment>
+                        <Form>
+                            <Form.Input onChange={(e, d) => setName(d.value)} value={name} placeholder='Name' />
+                            <Form.Input onChange={(e, d) => setEmail(d.value)} value={email} placeholder='Email' />
+                            <Form.Input onChange={(e, d) => setSubject(d.value)} value={subject} placeholder='Subject' />
+                            <TextArea onChange={(e, d) => setMessage(d.value)} value={message} class='field' placeholder='Message' style={{ height: '150px' }} />
+                            <Form.Button content={send} onClick={emailer} type='Submit' />
+                        </Form>
+                    </Segment>
+                </Grid.Column>
+                <Grid.Column width='10'>
+                    <Image src={require('../assets/pics/Lookout.jpg')} attached='top' />
+                </Grid.Column>
 
-        </Grid>
+            </Grid>
 
-    </>
-)
+        </>
+    )
 }
 
 export default Contact
