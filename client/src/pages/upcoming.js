96a4ff0ee22a7cc1588420525b7e09409322eb24
Quinn Calhoun <quinn.tcalhoun@gmail.com>
2020-07-31 21:03:14 -0700

    hot diggedy dog the by the power of resolution i commit thee

--- client/src/pages/upcoming.js
+++ client/src/pages/upcoming.js
@@ -4,7 +4,7 @@ const Upcoming = () => {
  
     return (
         <Container>
-            <img src={require('../assets/pics/appliedcontemp.webp')} alt='Applied Contemporary in Oakland' />
+            <img src={require('../assets/pics/appliedcontemp.jpeg')} alt='Applied Contemporary in Oakland' />
         </Container>
     )
 }
