c8abe989bd6ef048a21d932488771f58eb2961b2
Quinn Calhoun <quinn.tcalhoun@gmail.com>
2020-07-31 21:27:49 -0700

    i think we have done it. i would like to thank the academy, my mother, and the great angel Sretisla, who assisted me in the necessary blood sacrifice needed to appease Heroku

--- client/src/pages/upcoming.js
+++ client/src/pages/upcoming.js
@@ -1,10 +1,10 @@
 import React from 'react' 
-import { Container } from 'semantic-ui-react'
+import { Container, Image } from 'semantic-ui-react'
 const Upcoming = () => {
  
     return (
         <Container>
-            <img src={require('../assets/pics/appliedcontemp.jpeg')} style={{maxWidth: '350px'}} alt='Applied Contemporary in Oakland' />
+            <Image as='a' href='https://www.appliedcontemporary.com/current-exhibiiton' src={require('../assets/pics/appliedcontemp.jpeg')} style={{maxWidth: '350px'}} alt='Applied Contemporary in Oakland' />
         </Container>
     )
 }
