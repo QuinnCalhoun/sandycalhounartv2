0f368c3f1963b51acf36b9f9c2dac5182da26324
Quinn <quinn.tcalhoun@gmail.com>
2022-11-16 15:14:20 -0800

    cors fix 2

--- server.mjs
+++ server.mjs
@@ -1,7 +1,6 @@
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 const __dirname = dirname(fileURLToPath(import.meta.url))
-import cors from 'cors'
 import bodyParser from 'body-parser'
 import { MongoClient } from 'mongodb'
 
@@ -16,17 +15,12 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-const corsOptions = {
-  origin: (origin, callback) => {
-      callback(null, true);
-  },
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Access-Control-Allow-Origin", "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
-  credentials: true
-};
 
-app.options('*', cors(corsOptions));
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -35,13 +29,13 @@ if (process.env.NODE_ENV === "production") {
 
 import { router as routes } from "./routes/index.mjs"
 // Add routes, both API and view
-app.use(routes);
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
+app.use(routes);
 
 MongoClient.connect(process.env.MONGODB_URI, (err, db) => {
   if (err) {
